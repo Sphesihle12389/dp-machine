8276361b2402bcacdf5cf90fcdef16daeb149193
Sphesihle12389 <22312280@live.mut.ac.za>
2025-10-05 23:15:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ from lifelines import KaplanMeierFitter, CoxPHFitter
 from lifelines.utils import k_fold_cross_validation
 
 st.set_page_config(layout="wide", page_title="Two-Pot Retirement — Demo Dashboard")
-
+df=pd.read_csv("https://www.kaggle.com/datasets/pavansubhasht/ibm-hr-analytics-attrition-dataset?utm_source=chatgpt.com")
 # -------------------------
 # Helper utilities
 # -------------------------
